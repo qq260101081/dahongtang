f40dfc63ab01a5ef5f13b645342da531ae0c44d1
qq260101081 <260101081@qq.com>
2017-08-10 20:12:59 +0800

    Update SiteController.php
    配置发邮件

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -22,6 +22,15 @@ class SiteController extends Controller
         echo 1;die;
     }
 
-
+    public function actionSendEmail()
+    {
+        
+        $info = Yii::$app->mailer->compose()
+            ->setTo('260101081@qq.com')  //对方的邮箱
+            ->setSubject('yii2邮件发送')  
+            ->setTextBody('发送成功')  
+            ->send(); 
+        var_dump($info);
+    }
 
 }
